c84d2424de8ea621678308fa19ebd776c2c10b45
Christian Petzold <128407016+christianpetzold@users.noreply.github.com>
2025-07-26 23:21:39 +0200

    continued working on app bar and added Impressum

--- web-app/src/main.ts
+++ web-app/src/main.ts
@@ -2,6 +2,7 @@ import App from './App.vue'
 import {createApp} from 'vue'
 import vuetify from "@/plugins/vuetify.ts";
 import router from "@/router";
+import './assets/styles/main.css'
 
 if ('scrollRestoration' in history) {
     history.scrollRestoration = 'manual'
